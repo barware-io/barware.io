d7e16be0551b5259f2d25b74ca04747708e2ca1f
Orkhan <orkhanfarmanli@gmail.com>
2025-06-27 10:16:33 +0400

    Update next config

--- next.config.mjs
+++ next.config.mjs
@@ -10,8 +10,9 @@ const nextConfig = {
     unoptimized: true,
     path: isProd ? `/_next/image` : '',
     formats: ['image/avif', 'image/webp'],
-    minimumCacheTTL: 60,
+    minimumCacheTTL: 60 * 60 * 24 * 30, // 30 days
   },
+  compress: true
 };
 
 export default nextConfig;
